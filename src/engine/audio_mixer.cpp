720a48f874d5147a1de7aa5d2dd5247b5819b1f1
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-09-01 21:03:44 +0530

    add: Delete effect map explicitely after clearing

--- src/engine/audio_mixer.cpp
+++ src/engine/audio_mixer.cpp
@@ -1,5 +1,5 @@
 /**
- * audio_mixer.cpp - Audio manager
+ * audio_mixer.cpp - Audio mixer and manager utility
  * ------------------------------------------------------------------------
  *
  * Copyright (c) 2022-present Ajay Sreedhar
@@ -19,4 +19,93 @@
  * ========================================================================
  */
 
+#include "assert.hpp"
 #include "audio_mixer.hpp"
+
+#if defined(DINO_MODE_DEBUG) && DINO_MODE_DEBUG == 1
+#include "platform/logger.hpp"
+#endif
+
+
+dino::AudioMixer::AudioMixer() :
+        m_isLooping(false),
+        m_loopAudio(nullptr) {
+
+    m_effectMap = new std::map<unsigned int, Mix_Chunk*>();
+}
+
+dino::AudioMixer::~AudioMixer() {
+    if (m_isLooping) {
+        Mix_PauseMusic();
+    }
+
+    Mix_FreeMusic(m_loopAudio);
+
+    for (auto audio_it : *m_effectMap) {
+        Mix_FreeChunk(audio_it.second);
+    }
+
+    m_effectMap->clear();
+    delete m_effectMap;
+
+#if defined(DINO_MODE_DEBUG) && DINO_MODE_DEBUG == 1
+    dino::Logger::debug("Audio mixer destroyed.");
+#endif
+}
+
+void dino::AudioMixer::loadEffectAudio(unsigned int effect_id, const std::string &audio_file) {
+    auto mix_chunk = Mix_LoadWAV(audio_file.c_str());
+    DINO_ASSERT_SDL_HANDLE(mix_chunk, dino::EngineError::E_TYPE_MIX_RESULT)
+
+    m_effectMap->insert(std::pair(effect_id, mix_chunk));
+}
+
+void dino::AudioMixer::pauseLoopAudio() {
+    if (m_isLooping) {
+        Mix_PauseMusic();
+        m_isLooping = false;
+    }
+}
+
+void dino::AudioMixer::loadLoopAudio(const std::string &audio_file) {
+    pauseLoopAudio();
+
+    if (m_loopAudio != nullptr) {
+        Mix_FreeMusic(m_loopAudio);
+    }
+
+    m_loopAudio = Mix_LoadMUS(audio_file.c_str());
+    DINO_ASSERT_SDL_HANDLE(m_loopAudio, dino::EngineError::E_TYPE_MIX_RESULT)
+
+    m_isLooping = false;
+}
+
+void dino::AudioMixer::playLoopAudio() {
+    if (m_isLooping || m_loopAudio == nullptr){
+        return void();
+    }
+
+    int result = Mix_PlayMusic(m_loopAudio, -1);
+    DINO_ASSERT_SDL_RESULT(result)
+
+    m_isLooping = true;
+}
+
+void dino::AudioMixer::playEffectAudio(unsigned int effect_id) {
+    Mix_Chunk* mix_chunk;
+
+    try {
+        mix_chunk = m_effectMap->at(effect_id);
+
+        if (mix_chunk == nullptr) {
+            return void();
+        }
+    } catch (std::out_of_range& /* ignored */) {
+#if defined(DINO_MODE_DEBUG) && DINO_MODE_DEBUG == 1
+        dino::Logger::debug("Effect audio", effect_id, "does not exist.");
+#endif
+        return void();
+    }
+
+    Mix_PlayChannel(-1, mix_chunk, 0);
+}
--- src/engine/audio_mixer.hpp
+++ src/engine/audio_mixer.hpp
@@ -1,5 +1,5 @@
 /**
- * audio_mixer.hpp - Audio manager
+ * audio_mixer.hpp - Audio mixer and manager utility
  * ------------------------------------------------------------------------
  *
  * Copyright (c) 2022-present Ajay Sreedhar
@@ -20,3 +20,78 @@
  */
 
 #pragma once
+
+#include <map>
+#include <string>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+
+namespace dino {
+
+/**
+ * @brief Provides audio mixing and managing utilities.
+ */
+class AudioMixer {
+
+private:
+    /**
+     * @brief Determines if the BGM is looping.
+     */
+    bool m_isLooping;
+
+    /**
+     * @brief Holds the BGM bytes.
+     */
+    Mix_Music* m_loopAudio;
+
+    /**
+     * @brief Holds additional sound effects mapped by an identifier.
+     */
+    std::map<unsigned int, Mix_Chunk*>* m_effectMap;
+
+public:
+    /**
+     * @brief Initialises the instance.
+     */
+    AudioMixer();
+
+    /**
+     * @brief Cleans up when an instance is destroyed.
+     *
+     * This will pause the BGM and destroys all the loaded effects.
+     */
+    ~AudioMixer();
+
+    /**
+     * @brief Pauses the BGM.
+     */
+    void pauseLoopAudio();
+
+    /**
+     * @brief Loads an audio file and sets as game BGM.
+     * @param audio_file Absolute path to the WAV file.
+     * @throw dino::EngineError Thrown if audio loading fails.
+     */
+    void loadLoopAudio(const std::string&);
+
+    /**
+     * @brief Plays the loaded BGM infinitely or until paused.
+     */
+    void playLoopAudio();
+
+    /**
+     * Loads an audio file and maps it as an effect with and identifier.
+     * @param effect_id Numeric identifier to identify this audio.
+     * @param audio_file Absolute path to the WAV file.
+     * @throw dino::EngineError Thrown if audio loading fails.
+     */
+    void loadEffectAudio(unsigned int, const std::string&);
+
+    /**
+     * @brief Plays a sound effect identified by an id.
+     * @param effect_id The sound effect identifier.
+     */
+    void playEffectAudio(unsigned int);
+};
+
+} // namespace dino
