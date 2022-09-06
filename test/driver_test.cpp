39d85b2642f55c73c04e0ca8e8ccdbba2a93f776
Ajay Sreedhar <sree.ajay468@gmail.com>
2022-09-06 18:48:15 +0530

    add: Include filesystem module

--- test/driver_test.cpp
+++ test/driver_test.cpp
@@ -1,6 +1,11 @@
+#include "platform/filesystem.hpp"
+#include "platform/logger.hpp"
 #include "engine/graphics_driver.hpp"
 
 int main() {
+    dino::Logger::print(dino::Filesystem::resource("texture", "001.png"));
+    dino::Logger::print(dino::Filesystem::resource("audio", "001.wav"));
+
     dino::GraphicsDriver::initialise();
 
     dino::GraphicsDriver::printInfo();
