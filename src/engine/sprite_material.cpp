7b7596bfcd29b698a80d6c04aa3cffd79f6f4121
ajaysreedhar <sree.ajay468@gmail.com>
2024-02-14 14:08:00 +0530

    fix: Include  platform standard header

--- src/engine/sprite_material.cpp
+++ src/engine/sprite_material.cpp
@@ -29,14 +29,14 @@
 
 #include <vector>
 
+#include "assert.hpp"
+#include "sprite_material.hpp"
+
 #if defined (DINO_OS_TYPE_WINDOWS) && DINO_OS_TYPE_WINDOWS == 1
 #include <SDL_image.h>
 #elif defined (DINO_OS_TYPE_LINUX) && DINO_OS_TYPE_LINUX == 1 
 #include <SDL2/SDL_image.h>
 #endif // DINO_OS_TYPE_WINDOWS or DINO_OS_TYPE_LINUX
-
-#include "assert.hpp"
-#include "sprite_material.hpp"
 
 #if defined(DINO_MODE_DEBUG) && DINO_MODE_DEBUG == 1
 #include "platform/logger.hpp"
